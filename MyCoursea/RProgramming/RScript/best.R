754ee69b86e7099befac77479557ae58b48b3459
billni <niyong2008@gmail.com>
2014-08-15 14:07:54 +0800

    Update best.R

--- MyCoursea/RProgramming/RScript/best.R
+++ MyCoursea/RProgramming/RScript/best.R
@@ -8,14 +8,21 @@ best <- function(state, outcome) {
 	disease_name<-c("heart attack","heart failure","pneumonia")	
 	if(!any(disease_name==outcome)) stop("invalid outcome")
 	
-	num<-c(11,17,23)
-	names(num)<-disease_name
+	disease_col<-c(11,17,23)
+	names(disease_col)<-disease_name
 	
 	statelist <- which(hdata$State==state) 
-		
-	disease_data <- hdata[statelist,num[outcome]]
+	disease_data <- hdata[statelist,disease_col[outcome]]
+	names(disease_data) <- hdata$Hospital.Name[statelist]
+	
+	disease_data <- disease_data[disease_data != "Not Available"]
+	disease_name <- names(disease_data)
+	
 	disease_data <- as.numeric(disease_data)
-	names(disease_data) <- hdata$Hospital.Name[statelist]	
-  disease_data <- sort(disease_data)
+  names(disease_data) <- disease_name
+  
+	#names(disease_data) <- hdata$Hospital.Name[statelist]	
+	disease_data <- sort(disease_data)
   names(disease_data[1])	
 }
+
