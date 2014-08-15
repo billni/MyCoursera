08470f651bb4df4c01e43f81c3bcdef9e31ff695
billni <niyong2008@gmail.com>
2014-08-15 11:37:31 +0800

    Update best.R

--- MyCoursea/RProgramming/RScript/best.R
+++ MyCoursea/RProgramming/RScript/best.R
@@ -1,17 +1,21 @@
 best <- function(state, outcome) {
 	## Read outcome data
 	hdata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+	
 	## Check that state and outcome are valid
-	if (!any(hdata$State==state)) 	stop("invaild state")		
-	disease<-c("heart attack","heart failure","pneumonia")	
-	if(!any(disease==outcome)) stop("invaild outcome")
+	if (!any(hdata$State==state)) 	stop("invalid state")		
+	
+	disease_name<-c("heart attack","heart failure","pneumonia")	
+	if(!any(disease_name==outcome)) stop("invalid outcome")
 	
 	num<-c(11,17,23)
-	names(num)<-disease
+	names(num)<-disease_name
 	
 	statelist <- which(hdata$State==state) 
-	ill <- as.numeric(hdata[,num[outcome]][statelist])
-  names(ill) <- hdata$Hospital.Name[statelist]
-  ill=sort(ill)
-  names(ill[1])	
+		
+	disease_data <- hdata[statelist,num[outcome]]
+	disease_data <- as.numeric(disease_data)
+	names(disease_data) <- hdata$Hospital.Name[statelist]	
+  disease_data <- sort(disease_data)
+  names(disease_data[1])	
 }
