fe10ef09b6a864673bc7a403dbf1c19f7ccf0c6f
billni <niyong2008@gmail.com>
2014-08-19 09:53:25 +0800

    Update rankall.R

--- MyCoursea/RProgramming/RScript/rankall.R
+++ MyCoursea/RProgramming/RScript/rankall.R
@@ -12,18 +12,26 @@ rankall <- function(outcome, num="best"){
   names(result) <- c("hospital", "state", "ill", "rate")
       
   ## appear ill vector with respective name    
-	mortality <- c(11,17,23)
+	mortality <- c(11, 17, 23)
 	names(mortality) <- disease_name_list
 	      
 	## appear data frame with 3 columns , hospital_name , state name and mortality rate
 	amd <- hdata[, c(2, 7, mortality[outcome])]	   
-		
+	names(amd) <- c("Hospital", "State", "Rate")
+	flag <- "random"
   for (state in statelist) {
-    ## mortality data frame in each state     
-    esd <- amd[amd$State == state, ]        
-  	esd <- data.frame(esd, "Rate"=as.numeric(esd[,3]), stringsAsFactors=FALSE)   	
-		if (num=="worst") num <- nrow(esd)
-		if (num=="best") num <- 1	  	 
+    ## mortality data frame in each state
+    esd <- amd[amd$State == state, ]   
+   
+  	esd <- data.frame(esd, "Rate"=as.numeric(esd[,3]))   	
+		if (num=="worst") { 
+				flag <- "worst"
+				num <- nrow(esd)
+		}
+		if (num=="best") {
+		 		num <- 1	  	 
+		 		flag <- "best"
+		 }
   	
   	##order 4th and 1th column
   	esd <- esd[order(esd[,4], esd[,1]),]  	
@@ -32,10 +40,15 @@ rankall <- function(outcome, num="best"){
   	if (num <= nrow(esd)) {      	
   	   result[result$state==state,] <- esd[num,]
   	}
+  	
+  	if(flag=="worst") {
+  	   num <- "worst"
+  	} 
+  	if(flag=="best") {
+  	   num <- "best"
+  	}   	
   }    
+  result <- result[,c(1,2)]
   return(result)
+  
 }
-
-head(rankall("heart attack", 20), 10)
-tail(rankall("pneumonia", "worst"), 3)
-tail(rankall("heart failure"), 10)
